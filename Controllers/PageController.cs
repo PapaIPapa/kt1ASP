4c7d63e40f4388b43622be64a69f89283d743d1d
QuintMain <101810864+QuintMain@users.noreply.github.com>
2025-01-21 18:57:34 +0300

    Добавьте файлы проекта.

---
+++ Controllers/PageController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace kt1ASP.Controllers
+{
+    public class PageController : Controller
+    {
+        public IActionResult Welcome()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("Page/Greet/{name}")]
+        public IActionResult Greet(string name)
+        {
+            return View("Greet", name);
+        }
+
+        public IActionResult Edit()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(string message)
+        {
+            ViewBag.Message = message; 
+            return View("EditConfirmed");
+        }
+    }
+}
